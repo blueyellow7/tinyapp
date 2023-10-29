b7ce4c45c10f77b01133fcaaf3f1df40e9afc7a1
Neeharika Baral <neehabaral72@gmail.com>
2023-10-29 19:38:53 -0400

    added and edited comments

--- express_server.js
+++ express_server.js
@@ -21,40 +21,43 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
+// Display all urls (READ)
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index.ejs", templateVars);
 });
 
+// Display page to input new url (READ) 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new.ejs");
 });
 
+// Create new url. Redirect to its single url page (CREATE)
 app.post("/urls", (req, res) => {
   const id = generateRandomString(); 
-    // generate new short url/id (new key)
   urlDatabase[id] = req.body.longURL; 
-    // takes input from form (req.body = {longURL: new link input} ) and adds key/value pair to urlDatabase
+    // key = id, value = req.body.longURL -> add entry to urlDatabase object
   console.log(urlDatabase);
-    // make sure new longURL and new id/shortURL are added to database
   res.redirect(`/urls/${id}`); 
-    // when submit is clicked, redirect to urls/:id page to display new shortURL
 });
 
+// Display single url (READ)
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show.ejs", templateVars);
 });
 
+// Redirect to actual website from given shortened url (READ)
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id]
   res.redirect(longURL);
 });
 
+// Delete url in 'all urls page'. Redirect back to all urls page (DELETE)
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id]
   res.redirect("/urls");
@@ -63,3 +66,7 @@ app.post("/urls/:id/delete", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+// update user object eg.
+// users["5315"].first_name = "Jane"
