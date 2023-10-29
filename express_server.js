80b30bdad7b9a3058543b646dbdb8a1f4a216912
Neeharika Baral <neehabaral72@gmail.com>
2023-10-29 14:33:38 -0400

    Add a GET Route to Show the Form. Add a POST Route to Receive the Form Submission

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 5080; // default port 8080
-
-app.set("view engine", "ejs")
+const PORT = 8000;
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,6 +24,15 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index.ejs", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new.ejs");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
